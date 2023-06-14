962ac3ca7d2f9ce3673f57d5c2257af781c3eb97
Eugene-Kwaka <kwakaeugene@gmail.com>
2023-06-14 10:28:42 -0500

    How to use the doWhileLoop

---
+++ doWhileLoop.java
@@ -0,0 +1,12 @@
+public class doWhileLoop {
+    public static void main(String[] args) {
+        int number = 5;
+        int multiplier = 1;
+        do{
+            System.out.printf("%d X %d = %d \n", number, multiplier, number * multiplier);
+            multiplier++;
+        }
+        while(multiplier <= 10);
+        
+    }
+}
