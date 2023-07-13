71bd79140e9cbb9caa1a685a1e7079d6d369b75e
Eugene-Kwaka <kwakaeugene@gmail.com>
2023-07-12 19:02:56 -0500

    Continue statement in Java

---
+++ continueStatements.java
@@ -0,0 +1,12 @@
+public class continueStatements {
+    public static void main(String[] args){
+        for (int i = 0; i < 10; i++) {
+			// If the number is 2
+			// skip and continue
+			if (i == 2)
+				continue;
+
+			System.out.print(i + " ");
+		}
+    }
+}
