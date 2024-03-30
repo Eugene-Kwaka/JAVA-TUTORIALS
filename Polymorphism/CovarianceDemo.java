aa8fe80f65c03610f0738c200f2c7a477f1c6c11
Eugene-Kwaka <kwakaeugene@gmail.com>
2024-03-30 10:54:04 -0500

    Explaining covariance further

--- Polymorphism/CovarianceDemo.java
+++ Polymorphism/CovarianceDemo.java
@@ -3,13 +3,17 @@ public class CovarianceDemo {
         // Building a chair obj of type Chair with Furniture reference type
         Furniture chair = new Chair();
         Furniture table = new Table();
-        sell(chair);
-        sell(table);
+
+        //When the sell method takes an object of its actual subtype as its argument, it returns the subtype
+        sell(chair); // -> Chair@1f32e575
+        sell(table); // -> Table@279f2327
     }
    
+    // The sell method takes an object of supertype Furniture
     public static void sell (Furniture obj){
         System.out.println(obj);
     }
+    
 }
 
 class Furniture{}
